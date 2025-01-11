f85f1217bdf9d3f610eb28572c749c42e3007cea
umutt34788 <unutulanbardak@gmail.com>
2025-01-11 22:01:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [" /rumeta", " /rumeta'nın kölesiyim", " Rumeysaya aşığım🥺💘", " winery ve motmex tarafindan yapildim"];
+const statusMessages = [" wajahlcfyapfypaucpagu0", " YIOSA HATA YAP", " WTF BOY", " winery yapdi beni"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
