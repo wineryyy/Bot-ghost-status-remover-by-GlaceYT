2b6ef69e177586659e0e1f32940d7a72e35cb732
umutt34788 <unutulanbardak@gmail.com>
2025-01-12 19:01:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [" wajahlcfyapfypaucpagu0", " YIOSA HATA YAP", " WTF BOY", " winery yapdi beni"];
+const statusMessages = [" Created by BlueVotka", " /rumeta", " Rum", " Nabion"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
